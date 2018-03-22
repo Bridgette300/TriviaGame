0a17a0fbcd5080375bf78384cd44c2ebec37ab04
Bridgette Smith <bsmith@ift.org>
2018-03-21 20:28:17 -0500

    psuedocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,21 @@
-$( document).ready() {
+$(document).ready(function(){
 
 
+// need a timer or counter
 
-}
+
+//need to grab value of every anwser and store in variable
+
+//need to calculate which answer the user click on
+
+//need to calculate if that is the right answer
+
+//need to define right answer
+
+//when timer hits 0; Game over
+
+//game end screen should have correct and wrong answers
+
+//if all correct; game win screen
+
+});
